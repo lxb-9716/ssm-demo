0017e687f1aee93518d44666ce0e20f3ba8f550e
lxb <lxb_9716@163.com>
2021-03-09 17:42:03 +0800

    分支ssm_branch20210309代码做了修改

--- ssm/src/main/java/com/it/ssm/controller/TestController.java
+++ ssm/src/main/java/com/it/ssm/controller/TestController.java
@@ -15,6 +15,7 @@ public class TestController {
         System.out.println("这是我拉取的一个SSM框架demo整合的ssm_branch01分支，想看一下修改分支的代码，主分支的代码是否会改变");
         System.out.println("修改分支的代码，主分支的代码逻辑不受影响，分支拉取成功了，好开心！！！");
         System.out.println("我又拉取了一个新的分支ssm_branch20210309");
+        System.out.println("ssm_branch20210309分支代码做了修改");
         return "SSM框架整合测试成功!!!";
     }
 }
