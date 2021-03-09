8e840ddcda2651bb3ce75a31f4ec02247ad6c155
lxb <lxb_9716@163.com>
2021-03-09 16:30:10 +0800

    SSM框架整合demo，admin.sql文件为demo中对应的mysql数据库sql语句

---
+++ ssm/src/main/webapp/admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO `admin` (`id`, `userName`, `password`, `submitTime`) VALUES (1, '张三', '123456', '2021-3-9');
+INSERT INTO `admin` (`id`, `userName`, `password`, `submitTime`) VALUES (3, '李四3', '1234563', '2021-3-9');
+INSERT INTO `admin` (`id`, `userName`, `password`, `submitTime`) VALUES (5, '李四5', '1234565', '2021-3-9');
+INSERT INTO `admin` (`id`, `userName`, `password`, `submitTime`) VALUES (6, '李四6', '1234566', '2021-3-9');
+INSERT INTO `admin` (`id`, `userName`, `password`, `submitTime`) VALUES (7, '李四7', '1234567', '2021-3-9');
