56d1c692f6c8552aed8e7d0f3353706912ac63ef
lxb <lxb_9716@163.com>
2021-03-09 16:48:27 +0800

    SSM框架整合demo，修改分支的代码，主分支不会做任何改变，分支拉取成功了，好开心！！！

--- ssm/src/main/java/com/it/ssm/controller/TestController.java
+++ ssm/src/main/java/com/it/ssm/controller/TestController.java
@@ -13,6 +13,7 @@ public class TestController {
     @ResponseBody
     public String test() {
         System.out.println("这是我拉取的一个SSM框架demo整合的ssm_branch01分支，想看一下修改分支的代码，主分支的代码是否会改变");
+        System.out.println("修改分支的代码，主分支的代码逻辑不受影响，分支拉取成功了，好开心！！！");
         return "SSM框架整合测试成功!!!";
     }
 }
